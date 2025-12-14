f15ca2bb1db8f3087a3b418088a5a9ac565674d3
ifLauraAlmeida <oddlauraink@hotmail.com>
2025-12-14 18:39:57 -0300

    acrescentando cores

--- desafio002.py
+++ desafio002.py
@@ -1,3 +1,9 @@
 nome=input('Qual é o seu nome?')
+cores = {'limpa':'\033[m',
+        'azul':'\033[34m',
+        'amarelo':'\033[33m',
+        'pretoebranco':'\033[7;30m'}
+
 print('Olá, '+nome,'! Prazer em te conhecer!')   
+print(f'Olá, {cores["azul"]}{nome}{cores["limpa"]} ! Prazer em te conhecer!')
 print('Olá, {} l ! Prazer em te conhecer!'.format(nome) )
