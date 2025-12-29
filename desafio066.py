ee3e636e8c2e6064ee31f6fe0a74eaf295185f2d
ifLauraAlmeida <oddlaurainkl@hotmail.com>
2025-12-28 22:31:38 -0300

    aprendendo como usar o break no while

---
+++ desafio066.py
@@ -0,0 +1,12 @@
+# Exercício Python 66: Crie um programa que leia números inteiros pelo teclado. 
+# O programa só vai parar quando o usuário digitar o valor 999, que é a condição de parada. 
+# No final, mostre quantos números foram digitados e qual foi a soma entre elas (desconsiderando o flag).
+
+n = cont = soma = 0 
+while True:
+    n = int(input("Digite um número [999 para parar]: "))
+    if n == 999:
+        break
+    soma += n
+    cont+=1
+print(f"Você digitou {cont} números e a soma entre eles é {soma}.")
