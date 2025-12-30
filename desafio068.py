f49130e8b58086ae1c5be4972e0ec3204a11278f
ifLauraAlmeida <oddlauraink@hotmail.com>
2025-12-29 23:41:48 -0300

    finalização do projeto de jogo de impar e par com while

--- desafio068.py
+++ desafio068.py
@@ -6,48 +6,52 @@ import random as rd
 
 computador = 0
 jogador = 0
-print('''Suas opções:
-[ 1 ] ÍMPAR
-[ 2 ] PAR
-''')
+vitorias = 0
 
-jogador = int(input('Qual a sua jogada? '))
+while True:
+    print('''Suas opções:
+    [ 1 ] ÍMPAR
+    [ 2 ] PAR
+    ''')
+    jogador = int(input('Qual a sua jogada? '))
 
-if jogador == 1: # aqui o computador escolherá sempre o oposto dando prioridade para o usuário
-    jogador = "Ímpar"
-    computador = "Par"
-else:
-    jogador = "Par"
-    computador = "Ímpar"
-
-print('-=' * 20)
-print(f'Jogador escolheu {jogador}')
-print(f'Computador ficou com {computador}')
-print('-=' * 20)
-
-num_jogador = int(input("Escolha um número de 1 a 10: "))
-num_computador = rd.randint(0,11) # aqui o pc escolhe um número de 1 a 10
-print("-="*20)
-print(f"Você escolheu {num_jogador}.")
-print(f"O Computador escolheu {num_computador}.")
-resultado = num_jogador + num_computador
-print(f"{num_jogador} + {num_computador} = {resultado}")
-print("-="*20)
-
-print(f"LOGO:")
-
-if resultado / 2 == 0:
-    print(f"{resultado} É ÍMPAR")
-    if jogador == "Ímpar":
-        print("Jogador venceu!")
+    if jogador == 1: # aqui o computador escolherá sempre o oposto dando prioridade para o usuário
+        jogador = "Ímpar"
+        computador = "Par"
     else:
-        print("Computador venceu")
-else:
-    print(f"{resultado} É PAR")
-    if jogador == "Par":
-        print('Jogador venceu!')
+        jogador = "Par"
+        computador = "Ímpar"
+
+    print('-=' * 20)
+    print(f'Jogador escolheu {jogador}')
+    print(f'Computador ficou com {computador}')
+    print('-=' * 20)
+
+    num_jogador = int(input("Escolha um número de 1 a 10: "))
+    num_computador = rd.randint(0,11) # aqui o pc escolhe um número de 1 a 10
+    print("-="*20)
+    print(f"Você escolheu {num_jogador}.")
+    print(f"O Computador escolheu {num_computador}.")
+    resultado = num_jogador + num_computador
+    print(f"{num_jogador} + {num_computador} = {resultado}")
+    print("-="*20)
+
+    print(f"LOGO:")
+
+    if resultado / 2 == 0:
+        print(f"{resultado} É ÍMPAR")
+        if jogador == "Ímpar":
+            print("Jogador venceu!")
+            vitorias += 1
+        else:
+            print("Computador venceu")
+            break
     else:
-        print("Computador venceu!")
-
-
-
+        print(f"{resultado} É PAR")
+        if jogador == "Par":
+            print('Jogador venceu!')
+            vitorias += 1
+        else:
+            print("Computador venceu!")
+            break
+print(f"Você venceu {vitorias} vezes.")
