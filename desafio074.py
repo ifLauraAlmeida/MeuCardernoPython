1a2c89348287ac5362d08a888c71fbf6ab434886
ifLauraAlmeida <oddlauraink@hotmail.com>
2026-01-07 11:04:21 -0300

    max e min de 5 numeros aleatorios, usando a max() min() e random

--- desafio074.py
+++ desafio074.py
@@ -1,7 +1,12 @@
-# Exercício Python 074: Crie um programa que vai gerar cinco números aleatórios e colocar em uma tupla. 
-# Depois disso, mostre a listagem de números gerados e também indique o menor e o maior valor que estão na tupla.
+# Exercício Python 074: Crie um programa que vai gerar cinco números 
+# aleatórios e colocar em uma tupla. 
+# Depois disso, mostre a listagem de números gerados e também indique 
+# o menor e o maior valor que estão na tupla.
 from random import randint
 
-for c in range(5):
-    tupla = 
+numero = (randint(1,10), randint(1,10), randint(1,10), randint(1,10), randint(1,10))
 
+print(f'A listagem de números foi: {numero}')
+print(f'O maior número da lista foi: {max(numero)}')
+print(f'O menor número da lista foi: {min(numero)}')
+      
