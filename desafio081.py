bbb76c6ddaba82bffe12616418433a2ef3645ce7
ifLauraAlmeida <oddlauraink@hotmail.com>
2026-01-12 08:38:57 -0300

    desafio 81 de listas

---
+++ desafio081.py
@@ -0,0 +1,28 @@
+# Exercício Python 081: Crie um programa que vai ler vários números e colocar em uma lista.                  
+# Depois disso, mostre:                                                 
+# A) Quantos números foram digitados.                
+# B) A lista de valores, ordenada de forma decrescente.      
+# C) Se o valor 5 foi digitado e está ou não na lista.
+
+valores = []
+
+while True:
+    
+    valores.append(int(input('Digita um cacete dum valor ai: ')))
+    
+    resp = str(input('Deseja continuar [S/N]?')).upper().strip()[0]
+    
+    if 'S' not in resp:
+        break
+    
+print(f'Você digitou {len(valores)} elementos. ')
+print(f'Os valores em ordem decrescente são: {sorted(valores, reverse=True)}')  # reverse=True para inverter a ordem para decrescente
+
+# sort() → modifica a lista
+# sorted() → retorna uma nova lista
+
+if 5 in valores: #se colocar o 5 em aspas ele entende que é string
+    print('O valor 5 faz parte da lista! ')
+else:
+    print('O valor 5 não faz parte da lista!' )
+
