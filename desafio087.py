90061ea7847dc61940a6a276fea0f407fbea5d31
Seu Nome <oddlauraink@hotmail.com>
2026-01-17 13:40:38 -0300

    aprimorando o desafio087 com cores

--- desafio087.py
+++ desafio087.py
@@ -8,22 +8,23 @@ soma = 0
 
 for l in range(0,3):
     for c in range(0,3):
-        valor = int(input(f'Digite um valor para a linha {l} coluna {c}: '))
+        valor = int(input(f'\033[42mDigite um valor\033[m para a linha\033[1;32m {l} \033[mcoluna \033[1;32m{c}\033[m: '))
         matriz[l].append(valor)
         if matriz[l][c] % 2 == 0:
             soma += matriz[l][c]
-            
-print('-='*30)
+
+print()           
+print('\033[44m-=\033[m'*30)
 print()
-print(f'Esses são os valores da terceira coluna: ', end='')
-print(f'{matriz[2]}')
+print(f'Esses são os \033[4;34mvalores da terceira coluna\033[m: ', end='')
+print(f'\033[44m{matriz[2]}\033[m')
 print()
-print('-='*30)
+print('\033[44m-=\033[m'*30)
 print()
-print(f'A soma de todos os números pares é: {soma}')
+print(f'A \033[4;34msoma\033[m de todos os \033[4;34mnúmeros pares\033[m é: \033[44m{soma}\033[m')
 print()
-print('-='*30)
+print('\033[44m-=\033[m'*30)
 print()
-print(f'O maior valor da segunda linha da matriz é: {max(matriz[1])}')
+print(f'O \033[4;34mmaior valor da segunda linha da matriz\033[m é: \033[44m{max(matriz[1])}\033[m')
 print()
-print('-='*30)
+print('\033[44m-=\033[m'*30)
