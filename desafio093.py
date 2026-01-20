fd7dc2f00d0b13416b3b3547bd447e8076c81931
ifLauraAlmeida <oddlauraink@hotmail.com>
2026-01-20 18:14:41 -0300

    finalizando o desafio 93

--- desafio093.py
+++ desafio093.py
@@ -7,10 +7,25 @@
 jogador = dict()
 gols = list()
 
-jogador['nome'] = str(input('Nome do candango: '))
+jogador['nome'] = str(input('Nome do candango: ')).title()
 jogador['partidas'] = int(input('Quantas partidas? '))
 
-if jogador['partidas'] != 0:
-    for c in range(jogador['partidas']):
-        gols.append(int(input(f'Quantos gols na partida {c + 1}: ')))
-        print(gols)
+
+for c in range(jogador['partidas']):
+    gols.append(int(input(f'Quantos gols na partida {c + 1}: ')))
+jogador['total'] = sum(gols)
+jogador['goals'] = gols
+print()
+
+print('-='*30)
+print(jogador)
+print('-='*30)
+for k, v in jogador.items():
+    print(f'O campo {k} tem o valor {v}. ')
+print('-='*30)
+print(f'O jogador {jogador['nome']} jogou {jogador['partidas']} partidas.')
+for k in range(len(gols)):
+    print(f'   => Na partida {k}, fez {gols[k]} gols.')
+print(f'Foi um total de {jogador['total']}')
+
+
