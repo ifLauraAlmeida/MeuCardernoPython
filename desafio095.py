41c5919bbc4261b5a585bc35025d6b5b52d55716
ifLauraAlmeida <oddlauraink@hotmail.com>
2026-01-22 00:26:27 -0300

    finalizando desafio 95

--- desafio095.py
+++ desafio095.py
@@ -5,21 +5,42 @@ jogador = dict()
 jogadores = list()
 gols = list()
 
+jogador = {}
+jogadores = []
+gols = []
+
 while True:
+    jogador.clear()
+    gols.clear()
+
     jogador['nome'] = str(input('Nome: ')).title()
     jogador['partidas'] = int(input('Quantas partidas? '))
     
     for c in range(jogador['partidas']):
-        gols.append(int(input(f'Quantos gols na partida {c+1}: ')))
+        gols.append(int(input(f'   Quantos gols na partida {c+1}: ')))
         
     jogador['total'] = sum(gols)
-    jogador['goals'] = gols
-    jogadores.append(jogador)
-    print(jogadores)
+    jogador['gols'] = gols.copy()
+    jogadores.append(jogador.copy())
     
-    resp = str(input('Deseja prosseguir [S/N]? ')).upper()[0]
+    resp = str(input('=>Deseja prosseguir [S/N]? ')).upper()[0]
     if resp == 'N':
         break
-
     
-    for k in range
+print('-=' * 30)
+print(f'{"cod":<4}{"nome":<15}{"gols":<15}{"total":<5}')
+print('-' * 40)
+
+for i,j in enumerate(jogadores):
+    print(f'{i:<4}{j['nome']:<15}{str(j['gols']):<15}{j['total']:<5}')
+    
+while True:
+    busca = int(input('\nMostrar dados de qual jogador (999 para parar)? '))
+    if busca == 999:
+        break
+    if busca < 0 or busca >= len(jogadores):
+        print('Jogador não encontrado.')
+    else:
+        print('\033[1m => LEVANTAMENTO JOGADOR\033[m')
+        for i, g in enumerate(jogadores[busca]['gols']):
+            print(f'   No jogo {i+1} fez {g} gols.')
